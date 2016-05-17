3201980328584fb84c3b03f8e2709216b7914bd0
OttkO <adrian_iankov@abv.bg>
2016-05-17 18:33:34 +0200

    Added tests.

---
+++ tests/nl/tue/database/MySqlConnectorTest.java
@@ -0,0 +1,23 @@
+package nl.tue.database;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by OttkO on 5/17/2016.
+ */
+public class MySqlConnectorTest {
+    @Test
+    public void getPathQueryLikeCount() throws Exception {
+        assertEquals(MySqlConnector.GetPathQueryLikeCount(),85);
+
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        assertEquals(MySqlConnector.GetAll().size(),1499);
+
+    }
+
+}
