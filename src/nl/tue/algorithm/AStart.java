ebf82d2186024be3d4d0042a54095d1310af16c2
den1den <den_1_den@hotmail.com>
2016-05-20 00:53:23 +0200

    First attempt at AStart

--- src/nl/tue/algorithm/AStart.java
+++ src/nl/tue/algorithm/AStart.java
@@ -1,11 +1,6 @@
 package nl.tue.algorithm;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by dennis on 19-5-16.
@@ -23,11 +18,11 @@ public class AStart implements Iterable<int[]> {
     }
 
     @Override
-    public Iterator<int[]> iterator() {
+    public AStartIterator iterator() {
         return new AStartIterator();
     }
 
-    public abstract class HeuristicEvaluator {
+    public static class HeuristicEvaluator {
         private double heuristic = Double.NaN;
 
         public void setHeuristic(double heuristic) {
@@ -35,30 +30,122 @@ public class AStart implements Iterable<int[]> {
         }
     }
 
-    private class AStartIterator implements Iterator<int[]> {
-        Iterator<Integer> firstLevel;
-        Map<Integer, Integer> heuristics;
+    public class AStartIterator implements Iterator<int[]> {
 
         AStartIterator() {
-            this.firstLevel = Arrays.stream(labels).iterator();
-            this.heuristics = new HashMap<>();
+            ROOT = new Node(null, new int[]{}, Double.MAX_VALUE);
+            lastParent = ROOT;
+            ordering = new ArrayList<>(labels.length);
+            for (int i = 0; i < labels.length; i++) {
+                ordering.add(labels[i]);
+            }
         }
 
-        int level = 0;
+        List<Integer> ordering;
+        final Node ROOT;
+
+        TreeSet<Node> nexts = new TreeSet<>(cmpByHeuristic);
+
+        int maxLevel = 0;
+
+        Node lastParent;
+        int[] lastQuery = null;
 
         @Override
         public boolean hasNext() {
-            if (level == 0) {
-                if (firstLevel.hasNext()) {
-
-                }
-            }
-            throw new NotImplementedException();
+            return true;
         }
 
         @Override
         public int[] next() {
-            throw new NotImplementedException();
+            // First fill first level
+            if (maxLevel == 0) {
+                if (lastQuery == null) {
+                    // First node
+                    lastQuery = lastParent.next();
+                    return lastQuery;
+                }
+
+                nexts.add(new Node(lastParent, lastQuery, heuristicEvaluator.heuristic));
+                heuristicEvaluator.heuristic = Double.NaN;
+
+                if (lastParent.hasNext()) {
+                    lastQuery = lastParent.next();
+                    return lastQuery;
+                }
+
+                // First level complete
+                ordering.clear();
+                for (Node ordered : nexts) {
+                    ordering.add(ordered.subQuery[0]);
+                }
+
+                maxLevel++;
+            } else {
+                nexts.add(new Node(lastParent, lastQuery, heuristicEvaluator.heuristic));
+                heuristicEvaluator.heuristic = Double.NaN;
+            }
+
+            Node next = nexts.first();
+
+            while (!next.hasNext()) {
+                nexts.pollFirst();
+                next = nexts.first();
+            }
+
+            lastParent = next.parent;
+            lastQuery = next.next();
+
+            return lastQuery;
+        }
+
+        public int getLevel() {
+            return maxLevel;
+        }
+
+        public int getMaxLevel() {
+            return maxLevel;
+        }
+
+        public void setMaxLevel(int maxLevel) {
+            this.maxLevel = maxLevel;
+        }
+
+        private class Node implements Iterator<int[]> {
+            final Node parent;
+            final int[] subQuery;
+            final double heuristic;
+            int nxtLabelIndex = 0;
+
+            public Node(Node parent, int[] subQuery, double heuristic) {
+                if (Double.isNaN(heuristic)) {
+                    throw new AssertionError("parentHeuristic not set for query: " + Arrays.toString(subQuery));
+                }
+                this.parent = parent;
+                this.subQuery = subQuery;
+                this.heuristic = heuristic;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return nxtLabelIndex < ordering.size();
+            }
+
+            @Override
+            public int[] next() {
+                int nextLabel = ordering.get(nxtLabelIndex++);
+                int[] newQuery = new int[subQuery.length + 1];
+                System.arraycopy(subQuery, 0, newQuery, 0, subQuery.length);
+                newQuery[subQuery.length] = nextLabel;
+                return newQuery;
+            }
         }
     }
+
+    private static Comparator<AStartIterator.Node> cmpByHeuristic = new Comparator<AStartIterator.Node>() {
+        @Override
+        public int compare(AStartIterator.Node o1, AStartIterator.Node o2) {
+            return Double.compare(o1.heuristic, o2.heuristic);
+        }
+    };
 }
