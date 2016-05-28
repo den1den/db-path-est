0958cf6430d1aae274cf9dfc6af121876ab460cd
Nathan Cassee <Nathan Cassee>
2016-05-29 01:13:43 +0200

    Finished the test infrastructure that can be used to compare several methods.

--- tests/nl/tue/comparison/ComparisonExecutor.java
+++ tests/nl/tue/comparison/ComparisonExecutor.java
@@ -3,9 +3,16 @@ package nl.tue.comparison;
 import nl.tue.algorithm.Algorithm;
 import nl.tue.algorithm.Estimation;
 import nl.tue.algorithm.Estimator;
+import nl.tue.algorithm.NaiveJoinAlgorithm;
+import nl.tue.algorithm.pathindex.IndexQueryEstimator;
 import nl.tue.algorithm.pathindex.PathIndex;
+import nl.tue.algorithm.pathindex.PathSummary;
+import nl.tue.io.graph.AdjacencyList;
 import nl.tue.io.graph.DirectedBackEdgeGraph;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,20 +22,48 @@ import java.util.List;
  */
 public class ComparisonExecutor {
 
-    public static <E extends Estimation, A extends Estimator<E>> List<ComparisonResult>
-        executeComparisonsForPaths(List<int[]> paths,
-                                                                           Algorithm<E, A> method,
-                                                                    DirectedBackEdgeGraph graph) {
+    private static List<TestEnvironment> environments;
+
+    @BeforeClass
+    public static void before() {
+        environments = new ArrayList<>();
+
+        List<String> biblioQueries = Arrays.asList("+ 5 + 0 + 4 + 1 + 2", "+ 5 + 0 + 4 + 3",
+                "+ 5 + 0 + 1 + 2", "+ 0 + 4 + 1 + 2", "+ 5 + 0 - 4 + 1 + 2", "- 5 + 0 + 4 + 1 - 1",
+                "- 5 + 0 - 4 + 3", "+ 0 - 4 + 1 + 2");
+
+        File biblioFile = new File(AdjacencyList.class.getClassLoader().getResource("biblio.txt").getFile());
+
+        TestEnvironment biblio = new TestEnvironment(biblioQueries, biblioFile);
+
+        environments.add(biblio);
+    }
+
+    @Test
+    public void testNaiveIndexAndJoin() {
+        Algorithm<PathSummary, IndexQueryEstimator> algo = new NaiveJoinAlgorithm(new IndexQueryEstimator());
+
+        executeAndReportTests(algo, environments);
+    }
+
+    private static void executeAndReportTests(Algorithm<? extends Estimation, ? extends Estimator<? extends Estimation>> algo,
+                                       List<TestEnvironment> envs) {
         List<ComparisonResult> res = new ArrayList<>();
 
-        for(int[] path : paths) {
-            int estimation = method.query(path);
-
-            int result = graph.solvePathQuery(path).size();
-
-            res.add(new ComparisonResult(new PathIndex(path), result, estimation));
+        for(TestEnvironment env : envs) {
+            res.addAll(env.execute(algo));
         }
 
-        return res;
+        double percentageSum = 0;
+
+        for(ComparisonResult comparisonResult : res) {
+            percentageSum += comparisonResult.getAccuracy();
+        }
+
+        double accAverage = percentageSum / ((double)res.size());
+
+        System.out.println(String.format("Default method with dynamic joining of paths and naive joining " +
+                "has an accuracy of %s", accAverage));
     }
+
 }
---
+++ tests/nl/tue/comparison/TestEnvironment.java
@@ -0,0 +1,67 @@
+package nl.tue.comparison;
+
+import nl.tue.Main;
+import nl.tue.algorithm.Algorithm;
+import nl.tue.algorithm.Estimation;
+import nl.tue.algorithm.Estimator;
+import nl.tue.algorithm.pathindex.PathIndex;
+import nl.tue.io.Parser;
+import nl.tue.io.graph.AdjacencyList;
+import nl.tue.io.graph.DirectedBackEdgeGraph;
+
+import javax.management.Query;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Nathan on 5/29/2016.
+ */
+public class TestEnvironment {
+    private final List<String> queries;
+    private final File file;
+
+    public TestEnvironment(List<String> queries,
+                           File file) {
+        this.queries = queries;
+        this.file = file;
+    }
+
+    public List<ComparisonResult> execute(Algorithm<? extends Estimation, ? extends Estimator<? extends Estimation>> algo) {
+        Parser parser = new Parser();
+
+        try {
+            parser.parse(this.file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<int[]> intArrQueries = this.queries.stream().map(s -> Main.translateTextQueryToDomainQuery(s, parser)).
+                collect(Collectors.toList());
+
+        AdjacencyList graph = new AdjacencyList(parser);
+
+        algo.buildSummary(parser, 5, graph.getNodes().size() * 8);
+
+        return executeComparisonsForPaths(intArrQueries, algo, graph);
+    }
+
+    private static List<ComparisonResult>
+    executeComparisonsForPaths(List<int[]> paths, Algorithm<? extends Estimation,
+            ? extends Estimator<? extends Estimation>> method,
+                               DirectedBackEdgeGraph graph) {
+        List<ComparisonResult> res = new ArrayList<>();
+
+        for(int[] path : paths) {
+            int estimation = method.query(path);
+
+            int result = graph.solvePathQuery(path).size();
+
+            res.add(new ComparisonResult(new PathIndex(path), result, estimation));
+        }
+
+        return res;
+    }
+}
