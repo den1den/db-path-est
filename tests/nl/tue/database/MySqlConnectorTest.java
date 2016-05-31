24df9cc430ec12e88eb59c1b2f042200be5515b8
OttkO <adrian_iankov@abv.bg>
2016-05-31 09:24:59 +0200

    Added test functions for queries up to 5

--- tests/nl/tue/database/MySqlConnectorTest.java
+++ tests/nl/tue/database/MySqlConnectorTest.java
@@ -9,14 +9,28 @@ import static org.junit.Assert.assertEquals;
  */
 public class MySqlConnectorTest {
     @Test
-    public void getPathQueryLikeCount() throws Exception {
-        assertEquals(MySqlConnector.GetPathQueryLikeCount(),85);
+    public void getPathQueryThreeCount() throws Exception {
+        assertEquals(MySqlConnector.GetPathQueryThreeCount(0,4,3),85);
 
     }
+    @Test
+    public void getPathQueryTwoCount() throws Exception {
+        assertEquals(MySqlConnector.GetPathQueryTwoCount(0,4),293);
 
+    }
+    @Test
+    public void getPathQueryFourCount() throws Exception {
+        assertEquals(MySqlConnector.GetPathQueryFourCount(0,4,1,2),121);
+
+    }
+    @Test
+    public void getPathQueryFiveCount() throws Exception {
+        assertEquals(MySqlConnector.GetPathQueryFiveCount(0,4,1,2,3),0);
+
+    }
     @Test
     public void getAll() throws Exception {
-        assertEquals(MySqlConnector.GetAll().size(),1499);
+        assertEquals(MySqlConnector.GetAll().size(),2998);
 
     }
 
