f6b27286857aa6c7839d652f8a75aed06be9516f
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2016-06-08 23:43:15 +0200

    Added classes

---
+++ src/nl/tue/algorithm/subgraph/SubGraphAlgorithm_SF.java
@@ -0,0 +1,7 @@
+package nl.tue.algorithm.subgraph;
+
+/**
+ * Created by Dennis on 8-6-2016.
+ */
+public class SubGraphAlgorithm_SF extends SubGraphAlgorithm {
+}
---
+++ src/nl/tue/algorithm/subgraph/estimator/SubgraphEstimatorWSpecificFactors.java
@@ -0,0 +1,7 @@
+package nl.tue.algorithm.subgraph.estimator;
+
+/**
+ * Created by Dennis on 8-6-2016.
+ */
+public class SubgraphEstimatorWSpecificFactors extends SubgraphEstimator {
+}
