386c77b3872c44c3c9a492c9ec42d545dba8c521
OttkO <adrian_iankov@abv.bg>
2016-06-10 22:28:44 +0200

    Added simple datastructure.

---
+++ src/nl/tue/io/SomeDs.java
@@ -0,0 +1,14 @@
+package nl.tue.io;
+
+/**
+ * Created by OttkO on 6/9/2016.
+ */
+class SomeDs {
+    public int source,label, target = 0;
+    public SomeDs(int Source, int Label, int Target)
+    {
+        this.source = Source;
+        this.label = Label;
+        this.target = Target;
+    }
+}
