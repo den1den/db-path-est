dff6e862efe3632f6c13000cd0aa3cbd8636393e
OttkO <adrian_iankov@abv.bg>
2016-06-11 14:27:10 +0200

    Moved MergeZwei from Parser class to this one.

--- src/nl/tue/io/converters/GraphStructureConverter.java
+++ src/nl/tue/io/converters/GraphStructureConverter.java
@@ -1,18 +1,15 @@
 package nl.tue.io.converters;
 
 import nl.tue.io.TupleList;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by Dennis on 11-6-2016.
  */
 public class GraphStructureConverter {
     TupleList original;
-
+    private HashMap<Integer, ArrayList<Tuple>> incoming = new HashMap<>();
+    private HashMap<Integer, ArrayList<Tuple>> outgoing = new HashMap<>();
     public GraphStructureConverter(TupleList original) {
         this.original = original;
     }
@@ -56,6 +53,74 @@ public class GraphStructureConverter {
         }
         newTL.addAll(newTobeAdded);
         return newTL;
+    }
+    /**
+     * Keeps on merging nodes until they are reduced to @number
+     */
+    public void MergeZwei(int number) {
+        ArrayList<Tuple> nodesAndEdges = new ArrayList<>();
+        ArrayList<Integer> uniqueNodes = new ArrayList<>();
+
+        for (int[] input : this) {
+            if (!uniqueNodes.contains(input[0])) {
+                uniqueNodes.add(input[0]);
+            }
+            if (!uniqueNodes.contains(input[2])) {
+                uniqueNodes.add(input[2]);
+            }
+
+            nodesAndEdges.add(new Tuple(input[0], input[1], input[2]));
+
+        }
+
+        Iterator<Integer> keySetIterator = uniqueNodes.iterator();
+
+        while (keySetIterator.hasNext()) {
+            int key = keySetIterator.next();
+            ArrayList<Tuple> outg = new ArrayList<>();
+            ArrayList<Tuple> incom = new ArrayList<>();
+            for (Tuple ds : nodesAndEdges) {
+                if (ds.source == key) {
+                    outg.add(ds);
+                }
+                if (ds.target == key) {
+                    incom.add(ds);
+                }
+            }
+            outgoing.put(key, outg);
+            incoming.put(key, incom);
+
+        }
+
+
+        Random rnd = new Random();
+
+        while (uniqueNodes.size() > number) {
+            int firstIndex = rnd.nextInt(uniqueNodes.size() - 1);
+            int secondIndex = rnd.nextInt(uniqueNodes.size() - 1);
+
+            if (firstIndex != secondIndex) {
+
+                int one = uniqueNodes.get(firstIndex);
+                int two = uniqueNodes.get(secondIndex);
+                ArrayList<Tuple> nodeIncoming = incoming.get(one);
+                ArrayList<Tuple> nodeOutgoing = outgoing.get(one);
+                for (Tuple temp : nodeIncoming) {
+                    temp.target = two;
+                    incoming.get(two).add(temp);
+                }
+                for (Tuple temp : nodeOutgoing) {
+                    temp.source = two;
+                    outgoing.get(two).add(temp);
+                }
+
+                incoming.remove(one);
+                outgoing.remove(one);
+                uniqueNodes.remove(new Integer(one));
+
+            }
+
+        }
     }
 
 }
